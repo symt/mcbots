8bdd97b81280c79778812e85d08150383db3449d
symt <xxrequired@gmail.com>
2019-10-09 21:15:52 -0500

    Bunches of changes

--- index.js
+++ index.js
@@ -9,9 +9,42 @@ const figlet = require("figlet");
 const shell = require("shelljs");
 const mineflayer = require("mineflayer");
 const Vec3 = require("vec3").Vec3;
+var keypress = require('keypress');
+
+const autocompletions = ['loadbots', 'status', 'chat', 'control', 'registerbotevents', 'endbot', 'quit', 'stuck', 'togglechat'];
 
 let bots = [];
 let controlling;
+let chatToggled = false;
+let currentInput = "";
+
+process.stdin.on('keypress', function (ch, key) {
+    const autocomplete = (string) => {
+        let options = [];
+        autocompletions.forEach((completion) => {
+            if (completion.toLowerCase().startsWith(string.toLowerCase())) {
+                options.push(completion.substr(string.length));
+            }
+        });
+        return (options.length === 1) ? options[0] : '';
+    };
+    if (key) {
+        if (key.name === 'enter' || key.name === 'return') {
+            currentInput = "";
+        } else if (key.name === 'backspace') {
+            if (currentInput.length != 0)
+                currentInput = currentInput.slice(0, -1);
+        } else if (key.name !== 'tab') {
+            currentInput += ch;
+        } else {
+            const autocompleteTab = currentInput.split(" ");
+            currentInput += autocomplete(autocompleteTab[autocompleteTab.length - 1]);
+        }
+    }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -68,6 +101,8 @@ const loadBots = async () => {
             account.bot.end();
         });
         bots = [];
+        chatToggled = false;
+        controlling = undefined;
     }
 
     const botsFile = shell.exec(`cat ${FILEPATH}`, {
@@ -97,35 +132,38 @@ const loadBots = async () => {
                         version: VERSION,
                     }),
                     active: true,
+                    chatEnabled: false,
                     get registerEvents() {
                         return (() => {
-                            this.bot.on('chat', (username, message) => {
-                                console.log(`${username}: ${message}`);
-                            });
                             this.bot.on('error', (error) => {
                                 console.log(chalk.red("An error has occurred with ") + chalk.white(account.bot.username) + chalk.red(`: ${error.message}`));
                             });
                             this.bot.on('kicked', () => {
                                 this.active = false;
                             });
-                            // Of course, this doesn't work...
-                            this.bot._client.on('entity_velocity', v => {
-                                if (this.bot.entity.id !== v.entityId) return;
-                                this.bot.entity.velocity = new Vec3(v.velocityX / 256, v.velocityY / 512 * 1.5, v.velocityZ / 256);
+                        });
+                    },
+                    get registerChat() {
+                        return (() => {
+                            this.bot.on('message', (message) => {
+                                process.stdout.clearLine();
+                                process.stdout.cursorTo(0);
+                                console.log(message.toAnsi());
+                                process.stdout.write(` > ${currentInput}`);
                             });
                         });
-                    }
+                    },
                 });
-            }, 10000 * index);
+            }, 7500 * index);
             /* 
-             * This 10 second delay is to trick any servers that have anti-spam delays inacted. 
+             * This 7.5 second delay is to trick any servers that have anti-spam delays inacted. 
              * A future fix will be to use proxies, but that hasn't been implemented yet.
              */
         });
     }
 }
 
-const unFloat = () => {
+const stuck = () => {
     if (controlling && controlling.active) {
         controlling.bot.setControlState('jump', true);
         controlling.bot.setControlState('jump', false)
@@ -140,8 +178,34 @@ const unFloat = () => {
 const registerBotEvents = () => {
     bots.forEach((account) => {
         account.registerEvents();
-        console.log(chalk.green(`Events registered for ${account.bot.username}`));
+        console.log(chalk.green('Events registered for ') + chalk.gray(account.bot.username) + chalk.green('.'));
     });
+}
+
+const togglechat = (user) => {
+    let foundUser = false;
+    for (let account of bots) {
+        if (user !== "" && !user.includes(" ")) {
+            if (user.toLowerCase() === account.bot.username.toLowerCase()) {
+                if (!account.chatEnabled && !chatToggled) {
+                    account.registerChat();
+                    user = account.bot.username;
+                    chatToggled = true;
+                } else if (chatToggled && !account.chatEnabled) {
+                    console.log(chalk.red('Chat is already toggled.'));
+                } else if (chatToggled && account.chatEnabled) {
+                    console.log(chalk.red('Chat is already toggled for ') + chalk.gray(account.bot.username) + chalk.red('.'));
+                }
+                foundUser = true;
+                break;
+            }
+        }
+    }
+    if (!foundUser) {
+        console.log(chalk.red('Provide a user to toggle chat.'));
+    } else {
+        console.log(chalk.green('Chat has been enabled for ') + chalk.gray(user) + chalk.green('.'));
+    }
 }
 
 const endBot = (user) => {
@@ -149,32 +213,50 @@ const endBot = (user) => {
     for (let account of bots) {
         if (user !== "" && !user.includes(" ")) {
             if (user === account.bot.username) {
+                if (controlling && controlling === account) {
+                    controlling = undefined;
+                }
+                chatToggled = false;
                 account.bot.end();
                 account.active = false;
+                break;
             }
         } else {
+            if (controlling) {
+                controlling = undefined;
+            }
             account.bot.end();
             account.active = false;
         }
     }
+    if (user === "" || user.includes(" ")) {
+        chatToggled = false;
+    }
 }
 
 const status = () => {
-    bots.forEach((account) => {
-        console.log(chalk.cyan(account.bot.username) + chalk.blue(": ") + (account.active ? chalk.green("true") : chalk.red("false")));
-    });
+    if (bots.length === 0) {
+        console.log(chalk.red("There are no users to display. Do ") + chalk.gray("loadbots") + chalk.red(" to load up accounts."))
+    } else {
+        bots.forEach((account) => {
+            console.log(chalk.cyan(account.bot.username) + chalk.blue(": ") + (account.active ? chalk.green("true") : chalk.red("false")));
+        });
+    }
 }
 
 const control = (user) => {
     if (user === "control reset") {
         controlling = null;
+        console.log(chalk.green("Control was reset to everyone."))
     } else if (user.split(" ").length === 2) {
         for (let account of bots) {
             if (user.split(" ")[1].toLowerCase() === account.bot.username.toLowerCase()) {
                 controlling = account;
-                break;
+                console.log(chalk.green("Now controlling ") + chalk.gray(account.bot.username) + chalk.green("."))
+                return;
             }
         }
+        console.log(chalk.red("Could not find ") + chalk.gray(user.split(" ")[1].toLowerCase()) + chalk.red(". Please try a different user."));
     } else {
         console.log(chalk.red("Please provide a username to control."));
     }
@@ -196,7 +278,7 @@ const cmd = () => {
         name: 'cmd',
         prefix: '',
         message: '>',
-        autoCompletion: ['loadbots', 'status', 'chat', 'control', 'registerBotEvents', 'endbot', 'quit', 'unfloat'],
+        autoCompletion: autocompletions,
         context: 0,
         short: false
     }]).then(answers => {
@@ -236,8 +318,11 @@ const run = async () => {
             case "endbot":
                 endBot(response.substr(7));
                 break;
-            case "unfloat":
-                unFloat();
+            case "stuck":
+                stuck();
+                break;
+            case "togglechat":
+                togglechat(response.substr(11));
                 break;
             case "quit":
             case "exit":
